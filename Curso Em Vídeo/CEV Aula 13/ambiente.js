f84b5c962a91a124023ab950e132c269360aee13
AdemirRamos <ademirramos634@gmail.com>
2021-11-18 11:37:45 -0300

    Atualização da Aula 13

--- Curso Em Vídeo/CEV Aula 13/ambiente.js
+++ Curso Em Vídeo/CEV Aula 13/ambiente.js
@@ -1,6 +1,21 @@
 console.log('Tudo bem?')
 var c = 1
-do (c) {console.log(`Passo ${c}`) c++}
-while (c <= 6) 
-/*while (c <= 3) {console.log(`Passo ${c}`)}
-c++ // c++ == c = c + 1.*/
+while (c <= 6) {
+    console.log(`Passo ${c}.`)
+    c++
+}
+
+/*No caso do "Do While", primeiro o comando é executado, depois a condição é verificada.*/
+
+do {
+    console.log(`Passo ${c}.`)
+    c++
+}
+while (c <= 6)
+
+/*Repitições também podem ser chamadas de "laços" ou "interações".*/
+/*Estruturas de controle: sequência (sequencial); condições (condicional).*/
+
+//Nomes das Estruturas de Repetição vistas:
+//Estrutura de Repetição While: Estrutura de Repetição Com Teste Lógico No Ínicio.
+//Estrutura de Repetição Do While: Estrutura de Repetição Com Teste Lógico No Final.
