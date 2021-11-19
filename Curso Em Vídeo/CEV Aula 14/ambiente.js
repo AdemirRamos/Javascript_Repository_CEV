f29b833a8811568be4f9a0187499c09ab9d45c3f
AdemirRamos <ademirramos634@gmail.com>
2021-11-19 12:04:15 -0300

    Atualização da Aula 14

--- Curso Em Vídeo/CEV Aula 14/ambiente.js
+++ Curso Em Vídeo/CEV Aula 14/ambiente.js
@@ -1,12 +1,13 @@
-/*var c = 1
-while (c <= 10)
-    {console.log(c)
-    c++}*/
-
-/*for (var c = 1; c <= 5; c++)
-    {console.log(c)}*/
-
 console.log('Vai começar!')
-for (var c = 1; c <= 5; c++)
-    {console.log(c)}
+
+for (var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+
 console.log('Fim.')
+
+//Estrutura de Repetição Com Variável de Controle:
+//Nesse tipo de estrutura de repetição, são feitas três coisas:
+//1ª: Inicialização; 2ª: um teste lógico; 3ª: um incremento ou decremento.
+//Sendo o teste lógico verdadeiro, o bloco será executado, e, antes de repetir o processo, será feito um incremento.
+//Quando o teste lógico for falso, o programa seguirá com o seu fluxo.
