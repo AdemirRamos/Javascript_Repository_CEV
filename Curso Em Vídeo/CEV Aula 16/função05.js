a15c2ba98fee4a32560dc99e2dd76c34563d61bb
AdemirRamos <ademirramos634@gmail.com>
2021-11-20 03:22:48 -0300

    Atualização da Aula 16 (função05.js)

--- Curso Em Vídeo/CEV Aula 16/função05.js
+++ Curso Em Vídeo/CEV Aula 16/função05.js
@@ -1,12 +1,18 @@
-function fatorial (n)
-{if (n == 1) {return 1}
-else {n * fatorial(n-1)}}
-//O nome da linha de código acima é "recurssividade".
+function fatorial (n) {
+    if (n == 1) {
+        return 1
+    }
+    else {
+        return n * fatorial(n-1)
+    }
+}
+
+//Recursividade ocorre quando uma função chama a si própria.
 
 console.log(fatorial(5))
 
-/* 5! = 5 x 4 x 3 x 2 x 1 == 120.
-5 == 4!
-4! ==  4 x 3 x 2 x 1.
-n! == n x (n-1)!
-1! == 1.*/ 
+//5! = 5 x 4 x 3 x 2 x 1 == 120. - 5 fatorial.
+//5 == 4!. - "4!" == 4 fatorial.
+//4! ==  4 x 3 x 2 x 1.
+//n! == n x (n-1)!. - Essa é a fórmula para se encontrar o fatorial de um número.
+//1! == 1. - Esse é o fatorial de 1.
