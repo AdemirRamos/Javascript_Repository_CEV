4a31e475a9df9aaf6595483e1cf63589fc9efd76
AdemirRamos <ademirramos634@gmail.com>
2021-11-20 03:05:21 -0300

    Atualização da Aula 16 (função03.js)

--- Curso Em Vídeo/CEV Aula 16/função03.js
+++ Curso Em Vídeo/CEV Aula 16/função03.js
@@ -1,2 +1,6 @@
-let v = function (x) {return x*2}
-console.log(v(5))
+let v = function (x) { //Nesse caso, temos uma variável sendo uma função.
+    return x*2 //Nesse caso, o parâmetro "x" será multiplicado por dois.
+}
+
+console.log(v(5)) //À variável foi passado um parâmetro que, por sua vez, ao ser repassado à função, a executou.
+/*Existe uma síntaxe chamada "arrow function". Porém ela não será abordada aqui.*/
