3cf776fce86d3c42d53fcbc06b274c5e671d2814
AdemirRamos <ademirramos634@gmail.com>
2021-11-20 03:31:26 -0300

    Atualização da Aula 16 (função01.js) e Atualização da Aula 17

--- Curso Em Vídeo/CEV Aula 16/função01.js
+++ Curso Em Vídeo/CEV Aula 16/função01.js
@@ -12,7 +12,7 @@ let resultado = par_ou_impar(4) //O número entre parênteses é chamado de "par
 console.log(resultado) //Agora, após passar pela função, "resultado" tem o valor "Ímpar".
 
 //JS é uma linguagem de programação que também funciona sob o paradigma funcional, ou seja, JS também é uma linguagem funcional.
-//Porém, JS também é uma linguagem multi-paradigma.
+//Porém, JS também é uma linguagem multiparadigma.
 //Toda função pode possuir os seguintes elementos: uma chamada; um conjunto de parâmetros; uma ação; um retorno.
 //Nem toda função vai apresentar todos os elementos listados acima.
 //Funções: funções são ações executadas assim que são chamadas ou em decorrência de algum evento.
