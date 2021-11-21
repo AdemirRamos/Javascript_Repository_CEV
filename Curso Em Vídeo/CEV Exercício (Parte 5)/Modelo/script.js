212d59e89cfb6bb799757de499ba897a96e15b08
AdemirRamos <ademirramos634@gmail.com>
2021-11-21 13:18:38 -0300

    Atualização do Exercício (Parte 5)

--- Curso Em Vídeo/CEV Exercício (Parte 5)/Modelo/script.js
+++ Curso Em Vídeo/CEV Exercício (Parte 5)/Modelo/script.js
@@ -1,31 +1,31 @@
 function contar() {
-    let ini = document.getElementById('txti')
+    let ínicio = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
+    let resultado = document.getElementById('resultado')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        res.innerHTML = 'Impossível contar.'
-        //window.alert('[Erro] faltam dados.')
-    } else {
-        res.innerHTML = 'Contando: <br>'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(pass.value)
-        if (p <= 0) {
-            window.alert('Passo invávlido. Considerando Passo 1.')
-            p = 1   
-        }
-        if (1 < f) {
-            //Contagem crescente.
-            for (let c = 1; c <= f; c += p ){
-                res.innerHTML += ` ${c} \u{1F449}`
-            }
-        } else {
-            //Contagem regressiva.
-            for (let c = 1; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{1F449}`
-            }
-        }   res.innerHTML += `\u{1F3C1}`}
+    if (ínicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        resultado.innerHTML = 'É impossível contar pois faltam dados. Por favor, preencha todos os campos.'
+        //window.alert('Erro! Faltam dados.')
     }
-//Dentro das parênteses do "get elements by id", as aspas podem ser simples ou compostas.
+    else {
+        resultado.innerHTML = 'Contando: <br>'
+        let ini = Number(ínicio.value)
+        let end = Number(fim.value)
+        let pas = Number(passo.value)
+        if (pas <= 0) {
+            window.alert('Passo invávlido. Considerando Passo = 1.')
+            pas = 1   
+        }
+        if (ini < end) { //Contagem crescente.
+            for (let c = ini; c <= end; c += pas) {
+                resultado.innerHTML += ` ${c} \u{1F449}` //Emojis tirados da "Emoji List", funcionam dessa maneira (à esquerda) em JS.
+            }
+        }
+        else { //Contagem regressiva.
+            for (let c = ini; c >= end; c -= pas) {
+                resultado.innerHTML += `${c} \u{1F449}` //Esses códigos de emojis, em JS, só funcionam entre crases.
+            }
+        }   resultado.innerHTML += `\u{1F3C1}`
+    }
+}
