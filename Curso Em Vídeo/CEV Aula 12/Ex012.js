4fc58ce1d84facdb8ddebd9db85eb05c03a85ed9
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-01-10 23:32:26 -0300

    Atualização do Ex012
    Foram feitas algumas pequenas alterações no arquivo.

--- Curso Em Vídeo/CEV Aula 12/Ex012.js
+++ Curso Em Vídeo/CEV Aula 12/Ex012.js
@@ -1,9 +1,9 @@
 var agora = new Date()
 var hora = agora.getHours()
-/*Os dois comandos acima não necessários para se capturar a hora do sistema.*/
-/*Além de "getHours()", esse método também tem outras possibilidades. Basta digitar "get" e visualizá-las.*/
-/*A hora capturada vai ser determinada com base no sistema no qual o "script" está sendo executado.*/
-/*Se o "script" está sendo executado através do cliente, a hora mostrada será a do sistema do cliente - o mesmo vale para o servidor.*/
+//Os dois comandos acima são necessários para se capturar a hora do sistema.
+//Além de "getHours()", esse método também tem outras possibilidades. Basta digitar "get" e visualizá-las.
+//A hora capturada vai ser determinada com base no sistema no qual o "script" está sendo executado.
+//Se o "script" está sendo executado através do cliente, a hora mostrada será a do sistema do cliente - o mesmo vale para o servidor.
 
 console.log(`Agora são exatamente ${hora} horas.`)
 
