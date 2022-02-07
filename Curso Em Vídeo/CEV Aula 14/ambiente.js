2026a146c90ee4c4ae8ed0732b910652ebe102fa
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-02-07 20:20:21 -0300

    Atualização da Aula 14 - "ambiente.js"
    Fora feita apenas uma pequena mudança de cunho semântico na 11ª linha do documento.

--- Curso Em Vídeo/CEV Aula 14/ambiente.js
+++ Curso Em Vídeo/CEV Aula 14/ambiente.js
@@ -8,6 +8,6 @@ console.log('Fim.')
 
 //Estrutura de Repetição Com Variável de Controle:
 //Nesse tipo de estrutura de repetição, são feitas três coisas:
-//1ª: Inicialização; 2ª: um teste lógico; 3ª: um incremento ou decremento.
+//1ª: inicialização; 2ª: um teste lógico; 3ª: um incremento ou decremento.
 //Sendo o teste lógico verdadeiro, o bloco será executado, e, antes de repetir o processo, será feito um incremento.
 //Quando o teste lógico for falso, o programa seguirá com o seu fluxo.
